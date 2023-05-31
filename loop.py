6d87f8817c544658207dd67d2617442a533f24ae
abhishek-bhattarai <bhattaraiabhi@gmail.com>
2023-05-31 11:19:39 +0545

    second commit

---
+++ loop.py
@@ -0,0 +1,130 @@
+# for prashant in range(100):
+#     print(prashant)
+
+# for i in (0,10,2,2,3):
+#     print(i)
+
+
+# a=[1,2,3]
+# for i in a:
+#     print(i)
+
+# a=[1,2,3]
+# b=a
+# b[0]=5
+# print(a)
+# print(b)
+
+# a=[1,2,3]
+# b=copy.deepcopy(a)
+# b[0]=5
+# print(a)
+# print(b)
+
+# salary=[10000,20000,312334]
+# for i in salary:
+#     print("salary is",i*2)
+
+
+# info_dict= {
+#   "Name": "Ram",
+#   "Age": 20,
+#   "Country": "Nepal"
+# }
+# for key,value in info_dict.items():
+#     print(value)
+#     print(key)
+
+
+# for i in range(10,0,-1):
+#     print(i)
+# print("-------------------------------------")
+
+# for i in range(0,10,1):
+#     print(i)
+
+# # fruits = ["apple", "banana"]
+
+
+# print("even numbers between 1 to 100")
+# # for i in range(0,100,2):
+# #     print(i)
+#  print("-------------------------------------")
+
+# print("odd numbers between 1 to 100")
+# # for i in range(1,100,2):
+# #     print(i)
+
+
+# print("iterate over list and print the elements in for loop"}
+
+# list=[1,2,3,4,5]
+# for i in list:
+#     print(i)
+
+
+# touple=(1,2,3,4,5)
+# for i in touple:
+#     print(i)
+
+
+
+# info_student1= {
+#   "Name": "Ram",
+#   "Age": 20,
+#   "Country": "Nepal"
+# }
+# for key,value in info_student1.items():
+#     print(key,value)
+    
+    
+# string="this is python lab"
+# for i in string:
+#     print(i)
+
+
+# for i in range(0,3):
+#     for j in range(0,3):
+#         print(i,j)
+#     print(j,i)
+
+# for i in range (1,10):
+#     print(f"")
+#     for j in range (1,10):
+#         print(f"{i}x{j}={i*j}")
+
+
+# name=input("enter the name")
+# if name=="ram":
+#     print("hello ram")
+# elif name=="shyam":
+#     print("hello shyam")
+# elif name=="hari":
+#     print("hello krishna")
+# elif name=="gopal":
+#     print("hello gopal")
+# elif name=="sarada":
+#     print("hello sarada")abh
+# else: 
+#     print("hello stranger")
+
+
+
+# name=input("enter the name")
+# name_check=["ram"]
+# if name in name_check:
+#     print(f"hello {name}")
+# else:
+#     print("hello stranger")
+
+
+# while True:
+#     name=input("enter the name")
+#     name_check=["ram","shyam","hari"]
+#     if name in name_check:
+#         print(f"hello {name}")
+#         break
+#     else:
+#         print("hello stranger")
+#         continue
+
